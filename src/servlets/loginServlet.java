df442ee769f8ea22b58f37312745fab658face9e
Yuliana <yuliana12gm@gmail.com>
2017-11-05 19:56:02 +0100

    Add files via upload

---
+++ src/servlets/loginServlet.java
@@ -0,0 +1,56 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+@WebServlet("/loginServlet")
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		
+		if(userExists(username, password)) {
+			request.getSession().setAttribute("username", username);
+			response.sendRedirect("index.jsp");
+		}
+	}
+	
+	private boolean userExists(String _username, String _password) {
+		//Comrpobacion de usuario por conexion a la base de datos mediane Spring, JPA o JDBC
+		if(_username.equals("test")) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
